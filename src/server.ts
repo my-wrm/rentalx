9ff39711255ca3602f5de72e378feef5ef2ee9cb
Leonardo M. Primieri <leonardo.primieri@sof.to>
2022-09-24 20:58:56 -0300

    feat: add specification route

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express, { json } from "express";
-import { categoriesRoutes } from "./routes/categories.routes";
+import { categoriesRoutes, specificationsRoutes } from "./routes";
 
 const app = express();
 
 app.use(json());
 
 app.use("/categories", categoriesRoutes);
+app.use("/specifications", specificationsRoutes);
 
 app.listen(3333, () => console.log("Server is running! 🚀"));
