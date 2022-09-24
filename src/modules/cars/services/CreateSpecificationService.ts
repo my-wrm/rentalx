ee5faef73a74aa49248cbb508f2cd42520a38a64
Leonardo M. Primieri <leonardo.primieri@sof.to>
2022-09-24 20:47:10 -0300

    refactor: create card module

---
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -0,0 +1,23 @@
+import { ICategoryRepository } from "../repositories/ICategoriesRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateSpecificationService {
+  constructor(private categoryRepository: ICategoryRepository) {}
+
+  execute({ description, name }: IRequest): void {
+    const categoryAlreadyExists = this.categoryRepository.findByName(name);
+
+    if (categoryAlreadyExists) throw new Error("Category already exists!");
+
+    this.categoryRepository.create({
+      description,
+      name,
+    });
+  }
+}
+
+export { CreateSpecificationService };
