177274cc2ea446871542dfb27ab62625fb944ec8
Leonardo M. Primieri <leonardo.primieri@sof.to>
2022-09-26 22:39:42 -0300

    feat: add ormconfig

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express, { json } from "express";
 import { router } from "./routes";
 import swaggerUi from "swagger-ui-express";
 import swaggerFile from "./swagger.json";
+import "./database";
 
 const app = express();
 
