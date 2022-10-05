74ac253559cb42c87a4dbc99ac2cf01265081f60
Leonardo M. Primieri <leonardo.primieri@sof.to>
2022-10-04 23:35:59 -0300

    feat: create singleton for SpecificationRepository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,15 @@
 import { container } from "tsyringe";
 import { CategoryRepository } from "../../modules/cars/repositories/category-repository";
 import { ICategoryRepository } from "../../modules/cars/repositories/implementations/ICategoriesRepository";
+import { ISpecificationRepository } from "../../modules/cars/repositories/implementations/ISpecificationRepository";
+import { SpecificationRepository } from "../../modules/cars/repositories/specification-repository";
 
 container.registerSingleton<ICategoryRepository>(
   "CategoryRepository",
   CategoryRepository
+);
+
+container.registerSingleton<ISpecificationRepository>(
+  "SpecificationRepository",
+  SpecificationRepository
 );
